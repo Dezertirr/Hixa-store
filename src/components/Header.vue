3ddde513aa382f6914fd956d6eab4a383d1c6909
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-07-08 10:50:54 +0300

    Working Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,1 +1,72 @@
-vb
+<script setup>
+import { ref } from 'vue';
+
+const search = ref('');
+
+const searchStart = (event) => {
+  event.preventDefault();
+  console.log('Hi');
+};
+</script>
+<template>
+    <header>
+  <ul>
+        <li>Наша адреса</li>
+        <li>Час роботи</li>
+        <li>Доставка</li>
+        <li>Оплата</li>
+        <li>Відгуки</li>
+    </ul>
+    <ul>
+        <li>Мова</li>
+        <li>Валюта</li>
+        <li>Особистий кабінет</li>
+    </ul>
+    <div>
+        <div>logo</div>
+        <div><form>
+            <input :value="search" @input="event => search = event.target.value">
+          <button v-on:click="searchStart($event)">Search</button>
+
+    </form>
+  </div>
+        
+        <div>basket logo</div>
+    </div>
+    <ul>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <select name='Каталог'>
+        <option>Audi</option>
+        <option>BMW</option>
+        <option>Ser</option>
+    </select>
+    <li>Курс $</li>
+    </ul>
+</header>
+</template>
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  
+    <Header></Header>
+  
 </template>
